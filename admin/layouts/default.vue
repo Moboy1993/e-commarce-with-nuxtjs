3f14db11712fa59721785388ded0747eec3511ed
Sawmar123 <muratozkanboy27@gmail.com>
2022-12-22 17:20:23 +0300

    product create page designed admin page is not ready

---
+++ admin/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+  <TheNavbar/>
+  <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "../components/theNavbar.vue"
+export default{
+components:{
+  TheNavbar
+}
+}
+
+</script>
